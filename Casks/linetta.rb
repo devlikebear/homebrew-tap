0b1a0177f16f7391fd709071c92d299cc186bac4
mark42 <devlikebear@gmail.com>
2026-06-01 18:48:24 +0900

    feat: add linetta cask (v0.1.0)

---
+++ Casks/linetta.rb
@@ -0,0 +1,32 @@
+cask "linetta" do
+  version "0.1.0"
+  sha256 "feeb95a884c5c005b44cb5fb4dd60d9bed311920a01e2a4b80935351522253ea"
+
+  url "https://github.com/devlikebear/linetta/releases/download/v#{version}/Linetta-macos.app.tar.gz"
+  name "Linetta"
+  desc "Local-first desktop writing app for long-form fiction"
+  homepage "https://github.com/devlikebear/linetta"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  # The macOS build is published for Apple Silicon only.
+  depends_on arch: :arm64
+
+  app "Linetta.app"
+
+  zap trash: "~/Library/Application Support/com.devlikebear.linetta"
+
+  caveats <<~EOS
+    Linetta is ad-hoc signed and not notarized, so macOS Gatekeeper may block
+    it on first launch. To allow it to run, clear the quarantine attribute:
+
+      xattr -dr com.apple.quarantine "#{appdir}/Linetta.app"
+
+    Or install without quarantine in the first place:
+
+      brew install --cask --no-quarantine linetta
+  EOS
+end
